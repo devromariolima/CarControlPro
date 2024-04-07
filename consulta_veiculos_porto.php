daaea4ebf3ef0f5679e5106535cf0b3dd7cd44e6
Romário Lima <devromariolima@gmail.com>
2024-04-07 14:37:15 -0300

    criação do form de cadastro e edição para a cidade de porto dos gauchos

--- consulta_veiculos_porto.php
+++ consulta_veiculos_porto.php
@@ -7,7 +7,7 @@ $result = $mysqli->query($sql);
 // print_r($result);
 
 
-
+// header('Location:consulta_veiculos_porto.php')
 ?>
 
 
@@ -57,7 +57,7 @@ $result = $mysqli->query($sql);
                 Veiculos
               </a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="form_cadastro.php">Cadastrar Veiculos</a>
+                <a class="dropdown-item" href="form_cadastro_porto.php">Cadastrar Veiculos</a>
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="#">Relatorios</a>
               </div>
@@ -136,17 +136,17 @@ $result = $mysqli->query($sql);
       echo "<td>".$userdata['vencimento_do_seguro']."</td>";
       echo "<td>".$userdata['vencimento_do_ipva']."</td>";
       echo "<td>
-      <a class='btn btn-primary btn-sm' href='form.php?id=$userdata[id]'>
+
+      <a class='btn btn-primary btn-sm p-sm-2' href='form_edit_porto.php?id=$userdata[id]'>
       <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil' viewBox='0 0 16 16'>
          <path d='M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325'/>
       </svg>
-      </a>
-      <a class='btn btn-primary btn-sm' href='form_cadastro.php'>
-      <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-journal-check' viewBox='0 0 16 16'>
-       <path fill-rule='evenodd' d='M10.854 6.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 8.793l2.646-2.647a.5.5 0 0 1 .708 0'/>
-       <path d='M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2'/>
-       <path d='M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1z'/>
-      </svg>
+    </a>
+    <a class='btn btn-danger btn-sm p-sm-2' href='delete.php?id=$userdata[id]'>
+      <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash3 ' viewBox='0 0 16 16'>
+      <path d='M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5'/>
+    </svg>
+    </a>
    </td>";
 
    echo "</td>";
