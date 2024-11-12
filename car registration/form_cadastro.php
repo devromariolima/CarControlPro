477ee2fa9e7a89a878a838bbb80b0a70ebeddb87
Romário Lima <devromariolima@gmail.com>
2024-11-12 15:34:09 -0400

    Fix:form_cadastro.php Css/Style

--- car registration/form_cadastro.php
+++ car registration/form_cadastro.php
@@ -43,6 +43,7 @@ if (isset($_POST['submit'])) {
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
   <link rel="stylesheet" type="text/css" href="css/style_form.css" media="screen" />
+  <link rel="stylesheet" href="../css/form.css">
 
 </head>
 
